9f4e8069ee35ad17cdb6e3d7613e6349364d2406
Better Jeong <eunjeong42na@gmail.com>
2024-01-13 13:31:21 +0900

    initial commit

---
+++ src/main/java/tech/bread/solt/doctornyangserver/DoctorNyangServerApplication.java
@@ -0,0 +1,13 @@
+package tech.bread.solt.doctornyangserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorNyangServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoctorNyangServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/bread/solt/doctornyangserver/DoctorNyangServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.bread.solt.doctornyangserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorNyangServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
