e7a2b5407af3ac3aff8d7d967a33b84cc6475363
GiCheols <ngc4665@naver.com>
2024-04-09 19:11:55 +0900

    FEAT: 약물 복용 일정 보여주는 API 수정
    필요한 내용만 보여주도록 수정하였습니다.

--- src/main/java/tech/bread/solt/doctornyangserver/service/DosageService.java
+++ src/main/java/tech/bread/solt/doctornyangserver/service/DosageService.java
@@ -3,6 +3,7 @@ package tech.bread.solt.doctornyangserver.service;
 import tech.bread.solt.doctornyangserver.model.dto.request.DoneDosageRequest;
 import tech.bread.solt.doctornyangserver.model.dto.request.DosageRegisterRequest;
 import tech.bread.solt.doctornyangserver.model.dto.request.SetPrivateDosageRequest;
+import tech.bread.solt.doctornyangserver.model.dto.response.ShowDosageResponse;
 import tech.bread.solt.doctornyangserver.model.entity.Dosage;
 
 import java.util.List;
@@ -11,5 +12,5 @@ public interface DosageService {
     int registerDosage(DosageRegisterRequest request);
     Boolean toggleDosage(DoneDosageRequest request);
     int registerPrivateDosage(SetPrivateDosageRequest request);
-    List<Dosage> getMedicineDosage(int uid);
+    List<ShowDosageResponse> getMedicineDosage(int uid);
 }
