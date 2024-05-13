15a957de64058281777a22c0f6556498794452d0
Better Jeong <83108398+BetterJeong@users.noreply.github.com>
2024-05-13 16:04:25 +0900

    Update DoctorNyangServerApplication.java

--- src/main/java/tech/bread/solt/doctornyangserver/DoctorNyangServerApplication.java
+++ src/main/java/tech/bread/solt/doctornyangserver/DoctorNyangServerApplication.java
@@ -1,8 +1,11 @@
 package tech.bread.solt.doctornyangserver;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 
 @EnableScheduling
@@ -11,6 +14,11 @@ public class DoctorNyangServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DoctorNyangServerApplication.class, args);
+    }
+
+    @PostConstruct
+    public void setTimeZone(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
 }
