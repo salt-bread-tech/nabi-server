df99a9616d2b8dab59e04b8479216154dec00354
Better Jeong <83108398+BetterJeong@users.noreply.github.com>
2024-05-24 00:24:40 +0900

    FEAT: 프롬프트 수정

--- src/main/java/tech/bread/solt/doctornyangserver/util/GPTManager.java
+++ src/main/java/tech/bread/solt/doctornyangserver/util/GPTManager.java
@@ -14,60 +14,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GPTManager {
-    private String apiKey;
-    private String model;
-    private String url;
-    private List<GPTMessage> messages;
-    private Integer maxToken;
-    private Double temperature;
-    private Double topP;
-    private String authorization;
-    private String bearer;
-    private String mediaType;
+    private final String apiKey = KeySet.GPT_API_KEY.getKey();
+    private final String model = KeySet.GPT_MODEL.getKey();
+    private final String url = KeySet.GPT_URL.getKey();
 
-    private int type; // 1: 기본  2: 까칠
-
-    private static RestTemplate restTemplate = new RestTemplate();
+    private final List<GPTMessage> messages = new ArrayList<>();
+    private static final RestTemplate restTemplate = new RestTemplate();
 
     public GPTManager() {
-        apiKey = KeySet.GPT_API_KEY.getKey();
-        model = KeySet.GPT_MODEL.getKey();
-        url = KeySet.GPT_URL.getKey();
-
-        messages = new ArrayList<>();
-
-        type = 1;
-        initPersonality(type);
-
-        maxToken = 50;
-        temperature = 0.8;
-        topP = 1.0;
-
-        authorization = "Authorization";
-        bearer = "Bearer ";
-        mediaType = "application/json; charset=UTF-8";
+        initPersonality(1);
     }
 
     public GPTManager(List<Chat> recentConversations, int type) {
-        apiKey = KeySet.GPT_API_KEY.getKey();
-        model = KeySet.GPT_MODEL.getKey();
-        url = KeySet.GPT_URL.getKey();
-
-        messages = new ArrayList<>();
-
         initPersonality(type);
         applyRecentChats(recentConversations);
-
-        maxToken = 2000;
-        temperature = 0.8;
-        topP = 1.0;
-
-        authorization = "Authorization";
-        bearer = "Bearer ";
-        mediaType = "application/json; charset=UTF-8";
     }
 
     public ChatGPTResponse getResponse(String prompt) {
+        String mediaType = "application/json; charset=UTF-8";
+        String authorization = "Authorization";
+        String bearer = "Bearer ";
+
+        // GPT Parameter
+        Integer maxToken = 100;
+        Double temperature = 0.8;
+        Double topP = 0.8;
+        Double frequencyPenalty = 0.5;
+        Double presencePenalty = 0.5;
+
         messages.add(GPTMessage.builder()
                 .role("user")
                 .content(prompt)
@@ -80,10 +54,12 @@ public class GPTManager {
                 .maxTokens(maxToken)
                 .temperature(temperature)
                 .topP(topP)
+                .frequencyPenalty(frequencyPenalty)
+                .presencePenalty(presencePenalty)
                 .build();
+
         headers.setContentType(MediaType.parseMediaType(mediaType));
         headers.add(authorization, bearer + apiKey);
-
         HttpEntity<ChatGPTRequest> httpEntity = new HttpEntity<>(chatGPTRequest, headers);
         ResponseEntity<ChatGPTResponse> responseEntity = restTemplate.postForEntity(
                 url,
@@ -102,7 +78,7 @@ public class GPTManager {
                 .build());
         messages.add(GPTMessage.builder()
                 .role("system")
-                .content("사용자는 당신을 키우는 주인이며, 고양이를 키우는 집사입다." +
+                .content("사용자는 당신을 키우는 주인이며, 고양이를 키우는 집사입니다." +
                         "당신은 고양이지만, 사용자는 인간입니다. 사용자를 인간으로 대해야 합니다." )
                 .build());
         messages.add(GPTMessage.builder()
@@ -118,34 +94,29 @@ public class GPTManager {
                 .build());
         messages.add(GPTMessage.builder()
                 .role("system")
-                .content("대화는 반말로 하며, 말 끝마다 '냥'을 붙여야 한다." +
-                        "'사랑한다냥', '잘부탁해냥' 처럼 말 끝에 냥을 띄어쓰기 없이 붙여서 말해야 한다. " +
-                        "무슨 일이 있어도 문장 끝에 냥을 붙여야 한다. " )
+                .content("대화는 반말로 하며, 말 끝마다 '냥'을 붙여야 합니다." +
+                        "'사랑한다냥', '잘부탁해냥' 처럼 말 끝에 냥을 띄어쓰기 없이 붙여서 말해야 합니다. " +
+                        "무슨 일이 있어도 문장 끝에 냥을 붙여야 합니다. " )
                 .build());
         messages.add(GPTMessage.builder()
                 .role("system")
-                .content("사용자가 'ㅋ' 나 'ㅎ' 를 사용한다면 이것은 웃는 것이다. " +
-                        "사용자가 'ㅠ' 나 'ㅜ' 를 사용한다면 이것은 슬픈 감정이다. " )
+                .content("'안녕' 뒤에는 냥을 붙이면 안됩니다. '안냥' 으로 인사해야 합니다.")
                 .build());
         messages.add(GPTMessage.builder()
                 .role("system")
-                .content("'안녕' 뒤에는 냥을 붙이면 안된다. '안냥' 으로 인사해야 한다.")
-                .build());
-        messages.add(GPTMessage.builder()
-                .role("system")
-                .content("답변은 꼭 최대한 짧고 간결하게 대화 나누듯이 해야 한다." +
-                        "한 답변 내에서 같은 말을 반복하면 안 된다. " +
-                        "대화는 모두 한글로 하고, 절대 다른 언어를 사용하면 안된다. " )
+                .content("답변은 꼭 최대한 짧고 간결하게 대화 나누듯이 해야 합니다." +
+                        "한 답변 내에서 같은 말을 반복하면 안 됩니다. " +
+                        "대화는 모두 한글로 하고, 절대 다른 언어를 사용하면 안됩니다. " )
                 .build());
 
         switch (type) {
             case 2 -> messages.add(GPTMessage.builder()
                             .role("system")
-                            .content("너는 까칠한 고양이라서, 장난스럽고 까칠한 말투로 대답해야 한다.")
+                            .content("당신은 까칠한 고양이라서, 장난스럽고 까칠한 말투로 대답해야 합니다.")
                             .build());
             default -> messages.add(GPTMessage.builder()
                             .role("system")
-                            .content("너는 온순한 고양이라서, 집사에게 따뜻하고 착하게 대답해야 한다.")
+                            .content("당신은 온순한 고양이라서, 집사에게 따뜻하고 착하게 대답해야 합니다.")
                             .build());
         }
     }
