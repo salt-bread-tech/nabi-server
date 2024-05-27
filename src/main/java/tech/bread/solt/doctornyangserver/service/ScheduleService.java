a15c69e9d959cf937366e6306df0a89fd91fcafb
Better Jeong <83108398+BetterJeong@users.noreply.github.com>
2024-05-27 14:11:40 +0900

    FEAT: 스케줄 수정 API 추가

--- src/main/java/tech/bread/solt/doctornyangserver/service/ScheduleService.java
+++ src/main/java/tech/bread/solt/doctornyangserver/service/ScheduleService.java
@@ -1,8 +1,8 @@
 package tech.bread.solt.doctornyangserver.service;
 
+import tech.bread.solt.doctornyangserver.model.dto.request.ModifyScheduleRequest;
 import tech.bread.solt.doctornyangserver.model.dto.request.ScheduleRegisterRequest;
 import tech.bread.solt.doctornyangserver.model.dto.response.ScheduleListResponse;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
@@ -11,4 +11,5 @@ public interface ScheduleService {
     int register(ScheduleRegisterRequest request);
     boolean delete(int scheduleId);
     Map<LocalDate, List<ScheduleListResponse>> getScheduleList(LocalDate date, String id);
+    int modifySchedule(ModifyScheduleRequest request, String id);
 }
